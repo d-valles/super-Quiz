883d39c07e964d12a624adeaa27887fe28e53e2a
David Valles <david.valles@wizeline.com>
2018-03-05 13:25:43 -0600

    Added transformations

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,9 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
+                <transition name="flip" mode="out-in">
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                </transition>
         
             </div>
         </div>
@@ -43,3 +44,30 @@
     }
 </script>
 
+<style>
+    .flip-enter-active{
+        animate: flip-in 0.5s ease-out forwards;
+    }
+
+    .flip-leave-active{
+        animate: flip-out 0.5s ease-out forwards;
+    }
+
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0deg);
+        }
+        to {
+            transform: rotatey(90deg);
+        }
+    }
+
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            tramsform: rotateY(0deg);
+        }
+    }
+</style>
